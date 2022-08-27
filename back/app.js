8bb7423794681bf2a593cab60d37a014e15d0af8
S7-Enva <meneve.kevin@gmail.com>
2022-08-27 09:49:13 +0200

    Creation des routes et des controllers pour les sauces et user

--- back/app.js
+++ back/app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Sauce = require('./models/sauce');
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+const app = express();
 
 mongoose.connect('mongodb+srv://utilisateur1:1234@cluster0.gknawcy.mongodb.net/test',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-const app = express();
-
-app.use(express.json()); 
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,25 +22,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post ('/api/sauces', (req, res, next) => {
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauces', (req, res, next) => {
-  Sauce.find()
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes );
 
 module.exports = app;
---
+++ back/routes/sauces.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Sauce = require('../models/sauce');
+
+router.post ('/api/sauces', (req, res, next) => {
+    const sauce = new Sauce({
+      ...req.body
+    });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/api/sauces', (req, res, next) => {
+  Sauce.find()
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/api/stuff/:id', (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+});
+
+module.exports = router;
