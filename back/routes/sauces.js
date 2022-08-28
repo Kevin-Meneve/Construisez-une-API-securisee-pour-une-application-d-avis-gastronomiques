f35dd82766f500b9bbee8ed7a365f37b7c5174e3
S7-Enva <meneve.kevin@gmail.com>
2022-08-28 09:45:07 +0200

    Modifications des routes sauces

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const Sauce = require('../models/sauce');
 
-router.post ('/api/sauces', (req, res, next) => {
+router.post ('/', (req, res, next) => {
     const sauce = new Sauce({
       ...req.body
     });
@@ -13,13 +13,13 @@ router.post ('/api/sauces', (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
 });
 
-router.get('/api/sauces', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Sauce.find()
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(400).json({ error }));
 });
 
-router.get('/api/stuff/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({ error }));
