57b30ba2213d5ba4aba7d4b5d2e4c1c82480a44a
S7-Enva <meneve.kevin@gmail.com>
2022-08-29 09:31:28 +0200

    ajout des controllers et des routes pour les sauces

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const router = express.Router();
 
-const Sauce = require('../models/sauce');
+const sauceCtrl = require('../controllers/sauces');
 
-router.post ('/', (req, res, next) => {
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-});
+const Sauce = require('../models/sauce');// A supprimer après modif 
 
-router.get('/', (req, res, next) => {
-  Sauce.find()
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(400).json({ error }));
-});
 
-router.get('/:id', (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
+
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post ('/', auth, multer, sauceCtrl.createSauce);
+router.put ('/:id', auth, multer , sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post(':id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
